0afb088755da9d7f03aab3d1271b0eae255a8449
Fran <franrego333@gmail.com>
2024-09-25 13:28:55 +0200

    Ejercicio 4 Completado

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -7,7 +7,7 @@ public class Exercise04 {
         System.out.println("Ejemplos de cálculos SIN estructuras de control:");
         System.out.println("Suma de los primeros 5 números: " + (1 + 2 + 3 + 4 + 5));
         System.out.println("Suma de los primeros 20 números pares: " + (2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20 + 22 + 24 + 26 + 28 + 30 + 32 + 34 + 36 + 38 + 40));
-        System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2 * 1));
+        System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2));
         System.out.println("######################################################################");
         System.out.println("Ejemplos de cálculos CON estructuras de control:");
         System.out.println(sumNum(5));
